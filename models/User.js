6a1c3dbb920c1e8a8b51c56480aa06ac97d3e1d7
muddy700 <mohamedmfaume700@gmail.com>
2022-02-28 17:44:20 +0300

    feat: create user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const addressSchema = require("./AddressSchema");
+
+const userSchema = mongoose.Schema(
+  {
+    first_name: String,
+    last_name: String,
+    full_name: String,
+    dob: Date,
+    date_created: { type: Date, default: Date.now },
+    username: { type: String, required: true, unique: true },
+    email: String,
+    phone: String,
+    gender: String,
+    profile_image: String,
+    status: Boolean,
+    password: { type: String, required: true, minlength: 8 },
+    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+    address: { type: addressSchema },
+  },
+  { versionKey: false }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
