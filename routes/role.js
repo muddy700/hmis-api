69afd5742dc3b20df84c61728876a2dd4f46213b
muddy700 <mohamedmfaume700@gmail.com>
2022-02-28 12:35:49 +0300

    feat: add endpoint to create a new role

--- routes/role.js
+++ routes/role.js
@@ -16,3 +16,15 @@ router.get("/", async (req, res) => {
 });
 
 //Create new role
+router.post("/", async (req, res) => {
+  const role = new Role({
+    role_name: req.body.role_name,
+  });
+
+  try {
+    const response = await role.save();
+    res.status(200).send(role);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
