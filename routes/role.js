a195b1b99acbb3a9ac6991fc9fe14eb3d4a5f0fd
muddy700 <mohamedmfaume700@gmail.com>
2022-02-28 14:29:03 +0300

    feat: add endpoint to update a single role

--- routes/role.js
+++ routes/role.js
@@ -35,6 +35,29 @@ router.get("/:id", async (req, res) => {
     const role = await Role.findById(req.params.id);
     res.status(200).send(role);
   } catch (error) {
-    res.status(404).send(`No role found with id: ${req.params.id} `);
+    res.status(404).send({ error: `No role found with id: ${req.params.id} ` });
+  }
+});
+
+//Update Existing Role
+router.patch("/:id", async (req, res) => {
+  try {
+    //Check if document exists
+    const role = await Role.findById(req.params.id);
+
+    //Update only modified properties
+    if (req.body.role_name) role.role_name = req.body.role_name;
+
+    //Save changes
+    try {
+      const response = await role.save();
+      res.status(200).send(role);
+    } catch (error) {
+      //Throw error if failed to save changes
+      res.status(400).send({ error: error });
+    }
+  } catch (error) {
+    //Throw error if no document found
+    res.status(404).send({ error: `No role found with id: ${req.params.id} ` });
   }
 });
