f97f2d1d823259e2948aa637e8438ddc6a0ad8bc
muddy700 <mohamedmfaume700@gmail.com>
2022-02-28 17:48:08 +0300

    feat: import user router into index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const defaultRouter = require("./routes/index");
-const rolesRouter = require("./routes/role")
+const rolesRouter = require("./routes/role");
+const usersRouter = require("./routes/user");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
 //Get Configuration Variables
 dotenv.config();
@@ -15,11 +17,13 @@ mongoose
   .connect(db_url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     const app = express();
-    //   app.use(express.json())
-    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json()); //For JSON payload
+    app.use(express.urlencoded({ extended: true })); //For Form-Encoded
+    // app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use("/", defaultRouter);
     app.use("/api/v1/roles", rolesRouter);
+    app.use("/api/v1/users", usersRouter);
 
     app.listen(PORT, () => {
       console.log(`Server Connected And App Is Running On Port #: ${PORT}`);
