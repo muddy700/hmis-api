96b7337fa9b86b3ca9f4bae4a2bad1728fa2d4b2
muddy700 <mohamedmfaume700@gmail.com>
2022-03-02 11:04:27 +0300

    feat: compare user hashed passwords before generating token

--- routes/login.js
+++ routes/login.js
@@ -9,20 +9,27 @@ router.post("/", async (req, res) => {
   try {
     const user = await User.findOne({
       username: req.body.username,
-      password: req.body.password,
     });
 
     //Handle error when get null as response
     if (!user) return res.status(400).send("Incorrect Username or Password");
 
-    //If user found create token
-    try {
-      const token = await createToken({ userId: user._id });
-      res.status(200).send({token});
-    } catch (error) {
-      //Handle error when fail to generate token
-      res.status(500).send({ error: error });
-    }
+    //If user found, compare passwords
+    user.comparePasswords(req.body.password, async (matchError, isMatch) => {
+      if (matchError) return res.status(400).send({ error: matchError });
+      else if (!isMatch)
+        return res.status(400).send("Incorrect Username or Password");
+      else {
+        //If passwords match, create token
+        try {
+          const token = await createToken({ userId: user._id });
+          res.status(200).send({ token });
+        } catch (error) {
+          //Handle error when fail to generate token
+          res.status(500).send({ error: error });
+        }
+      }
+    });
   } catch (error) {
     //Handle error when fail to get user info
     res.status(400).send({ error: error });
