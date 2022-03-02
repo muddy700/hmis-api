ff1a05f45b0eb69049d82113e45ab0b0925ee464
muddy700 <mohamedmfaume700@gmail.com>
2022-03-02 17:11:52 +0300

    feat: add endpoint to create lab-test-template

--- routes/labTestTemplate.js
+++ routes/labTestTemplate.js
@@ -12,4 +12,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Create New LabTestTemplate
+router.post("/", async (req, res) => {
+  //Initialize new instance/doc
+  const labTestTemplate = new LabTestTemplate({
+    name: req.body.name,
+    price: req.body.price,
+  });
+
+  try {
+    const response = await labTestTemplate.save();
+    res.status(200).send(labTestTemplate);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
 module.exports = router
