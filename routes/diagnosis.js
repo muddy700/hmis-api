ff51825e845182860a8db915bc0b9c338094b558
muddy700 <mohamedmfaume700@gmail.com>
2022-03-02 16:11:35 +0300

    feat: add endpoint to get diagnosis by id

--- routes/diagnosis.js
+++ routes/diagnosis.js
@@ -27,5 +27,16 @@ router.post("/", async (req, res) => {
   }
 });
 
+//Get Diagnosis By Id
+router.get("/:id", async (req, res) => {
+  try {
+    const diagnosis = await Diagnosis.findById(req.params.id);
+    res.status(200).send(diagnosis);
+  } catch (error) {
+    res.status(404).send({ error: `No diagnosis found with id: ${req.params.id} ` });
+  }
+});
+
+
 
 module.exports = router
