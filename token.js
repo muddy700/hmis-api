e0b292f9170b3db9ccd070d80bcbd13a8f6c6669
muddy700 <mohamedmfaume700@gmail.com>
2022-03-02 09:50:19 +0300

    refactor: extends token expiry time

--- token.js
+++ token.js
@@ -5,7 +5,7 @@ dotenv.config();
 const secret_key = process.env.JWT_SECRET_KEY;
 
 const generateAccessToken = (userId) => {
-  const token = jwt.sign(userId, secret_key, { expiresIn: "90s" });
+  const token = jwt.sign(userId, secret_key, { expiresIn: "30m" });
   return token;
 };
 
