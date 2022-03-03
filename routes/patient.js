7a26671a727acc95bb9a66a07a8708403051d3c0
muddy700 <mohamedmfaume700@gmail.com>
2022-03-03 11:22:39 +0300

    feat: add endpoint to register new patient

--- routes/patient.js
+++ routes/patient.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const patientModule = require("../models/Patient");
 const Patient = patientModule.patientModel;
@@ -6,10 +6,23 @@ const Patient = patientModule.patientModel;
 //Get All Patients
 router.get("/", async (req, res) => {
   try {
-    const patients = await Patient.find().populate("practitioner");
+    const patients = await Patient.find();
     res.status(200).send(patients);
   } catch (error) {
     res.status(500).send({ error: error });
+  }
+});
+
+//Create New Patient
+router.post("/", async (req, res) => {
+  //Initialize new instance/doc
+  const patient = new Patient(req.body);
+
+  try {
+    const response = await patient.save();
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(400).send({ error: error });
   }
 });
 
