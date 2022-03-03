390f5affc4164e9ae51af1d80b87eb1a0129aef3
muddy700 <mohamedmfaume700@gmail.com>
2022-03-03 12:34:30 +0300

    feat: add endpoint to update patient info

--- routes/patient.js
+++ routes/patient.js
@@ -31,10 +31,60 @@ router.get("/:id", async (req, res) => {
   try {
     const patient = await Patient.findById(req.params.id).populate({
       path: "vital_signs",
-      populate: { path: "practitioner", select: 'full_name gender -_id' },
+      populate: { path: "practitioner", select: "full_name gender -_id" },
     });
     res.status(200).send(patient);
   } catch (error) {
+    res
+      .status(404)
+      .send({ error: `No patient found with id: ${req.params.id} ` });
+  }
+});
+
+//Update Existing Patient
+router.patch("/:id", async (req, res) => {
+  try {
+    //Check if patient exists
+    const patient = await Patient.findById(req.params.id);
+
+    //Ignore read only properties
+    const { full_name, date_registered, vital_signs, ...remainingData } =
+      req.body;
+
+    //Retrieve modified properties
+    const modifiedProperties = Object.keys(remainingData);
+
+    //Loop and update only properties with data
+    modifiedProperties.forEach((key, index) => {
+      if (!remainingData[key]) {
+        // console.log(`${key} has no value`);
+      } else if (key === "address") {
+        //Handle embedded doc
+        let oldAddress = patient[key];
+        const newAddress = remainingData[key];
+
+        if (!oldAddress) patient[key] = newAddress;
+        else {
+          //Loop and update only properties with data
+          Object.keys(newAddress).forEach((prop, index) => {
+            if (newAddress[prop]) oldAddress[prop] = newAddress[prop];
+          });
+        }
+      } else {
+        patient[key] = remainingData[key];
+      }
+    });
+
+    //Save changes
+    try {
+      const response = await patient.save();
+      res.status(200).send(response);
+    } catch (error) {
+      //Throw error if failed to save changes
+      res.status(400).send({ error: error });
+    }
+  } catch (error) {
+    //Throw error if no patient found
     res
       .status(404)
       .send({ error: `No patient found with id: ${req.params.id} ` });
