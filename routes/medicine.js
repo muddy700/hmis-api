f90eadb7e53670e40e10f0452d57461fdc679990
muddy700 <mohamedmfaume700@gmail.com>
2022-03-03 09:42:28 +0300

    feat: add endpoint to update single medicine

--- routes/medicine.js
+++ routes/medicine.js
@@ -37,5 +37,31 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//Update Existing Medicine
+router.patch("/:id", async (req, res) => {
+  try {
+    //Check if document exists
+    const medicine = await Medicine.findById(req.params.id);
+
+    //Update only modified properties
+    Object.keys(req.body).forEach((prop, index) => {
+      if (req.body[prop]) medicine[prop] = req.body[prop];
+    });
+
+    //Save changes
+    try {
+      const response = await medicine.save();
+      res.status(200).send(response);
+    } catch (error) {
+      //Throw error if failed to save changes
+      res.status(400).send({ error: error });
+    }
+  } catch (error) {
+    //Throw error if no document found
+    res
+      .status(404)
+      .send({ error: `No medicine found with id: ${req.params.id} ` });
+  }
+});
 
 module.exports = router;
