dab53d6b3b21ee07dfd95c2aa2bb772ce809ffa2
muddy700 <mohamedmfaume700@gmail.com>
2022-03-04 14:14:21 +0300

    feat: add endpoint to get all appintments

--- routes/appointment.js
+++ routes/appointment.js
@@ -2,4 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Appointment = require("../models/Appointment");
 
+//Get All Appointments
+router.get("/", async (req, res) => {
+  try {
+    const appointments = await Appointment.find();
+    res.status(200).send(appointments);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
 module.exports = router;
