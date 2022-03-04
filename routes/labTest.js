7b61ece67001b033827803c2b517b40a0a51446e
muddy700 <mohamedmfaume700@gmail.com>
2022-03-04 18:14:14 +0300

    feat: add endpoint to get all lab-tests

--- routes/labTest.js
+++ routes/labTest.js
@@ -2,4 +2,14 @@ const express = require("express");
 const router = express.Router();
 const LabTest = require("../models/LabTest");
 
+//Get All LabTest
+router.get("/", async (req, res) => {
+  try {
+    const labTests = await LabTest.find();
+    res.status(200).send(labTests);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
 module.exports = router;
