854a9097f8fd01ebe3ce2a70ab9bddd94dbea763
muddy700 <mohamedmfaume700@gmail.com>
2022-03-04 14:17:58 +0300

    feat: add endpoint to create new appointment

--- routes/appointment.js
+++ routes/appointment.js
@@ -12,4 +12,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Create New Appointment
+router.post("/", async (req, res) => {
+  //Initialize new instance/doc
+  const appointment = new Appointment(req.body);
+  
+  try {
+    const response = await appointment.save();
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
 module.exports = router;
