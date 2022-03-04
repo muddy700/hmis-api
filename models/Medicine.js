c288ed89d2fedd3e31a6468f5be3154cccd5ddc7
muddy700 <mohamedmfaume700@gmail.com>
2022-03-04 14:12:54 +0300

    fix: remove constraint

--- models/Medicine.js
+++ models/Medicine.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const medicineSchema = mongoose.Schema({
   drug_code: { type: String, required: true, unique: true },
   drug_name: { type: String, required: true, unique: true },
-  price: { type: Number, required: true, unique: true },
-  dosage: { type: String, required: true, unique: true },
+  price: { type: Number, required: true},
+  dosage: { type: String, required: true },
 }, {versionKey: false});
 
 const medicineModel = mongoose.model("Medicine", medicineSchema);
