9ca9c57ceec1cd2a5642fccef73b4165d89e9e8e
muddy700 <mohamedmfaume700@gmail.com>
2022-03-04 18:57:17 +0300

    feat: add reusable function to get labtest

--- routes/labTest.js
+++ routes/labTest.js
@@ -15,6 +15,33 @@ const patientPopulator = {
   select: "full_name -_id",
 };
 
+//Re-usable function for checking if labTest exists
+const findLabTest = async (req, res) => {
+  try {
+    //Check if labTest exists
+    const labTest = await LabTest.findById(req.params.id)
+      .populate(patientPopulator)
+      .populate(testTemplatePopulator)
+      .populate(labTechincianPopulator);
+
+    if (!labTest) {
+      //If got null response, return error and exit function
+      res
+        .status(404)
+        .send({ error: `No labTest found with id: ${req.params.id} ` });
+      return false;
+    } else {
+      return labTest;
+    }
+  } catch (error) {
+    //Throw error if no labTest found
+    res.status(404).send({
+      error: `No labTest found with id: ${req.params.id} `,
+    });
+    return false;
+  }
+};
+
 //Get All LabTest
 router.get("/", async (req, res) => {
   try {
