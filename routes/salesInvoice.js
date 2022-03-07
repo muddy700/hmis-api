00571660931c4f07ef35b31f3b11a73a93b9ca1d
muddy700 <mohamedmfaume700@gmail.com>
2022-03-07 12:27:29 +0300

    feat: add populators when retrieving sales invoices

--- routes/salesInvoice.js
+++ routes/salesInvoice.js
@@ -2,10 +2,28 @@ const express = require("express");
 const router = express.Router();
 const Salesinvoice = require("../models/SalesInvoice");
 
+const patientPopulator = {
+  path: "patient",
+  select: "full_name -_id",
+};
+
+const cashierPopulator = {
+  path: "cashier",
+  select: "full_name -_id",
+};
+
+const paymentModePopulator = {
+  path: "payment_mode",
+  select: "payment_mode_name -_id",
+};
+
 //Get All Sales Invoices
 router.get("/", async (req, res) => {
   try {
-    const salesInvoices = await Salesinvoice.find();
+    const salesInvoices = await Salesinvoice.find()
+      .populate(patientPopulator)
+      .populate(cashierPopulator)
+      .populate(paymentModePopulator);
     res.status(200).send(salesInvoices);
   } catch (error) {
     res.status(500).send({ error: error });
