40d0b26b627844b8448a8596ee06384cec45076a
muddy700 <mohamedmfaume700@gmail.com>
2022-03-07 11:17:49 +0300

    feat: add endpoint to get all sales invoices

--- routes/salesInvoice.js
+++ routes/salesInvoice.js
@@ -2,4 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Salesinvoice = require("../models/SalesInvoice");
 
+//Get All Sales Invoices
+router.get("/", async (req, res) => {
+  try {
+    const salesInvoices = await Salesinvoice.find();
+    res.status(200).send(salesInvoices);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
 module.exports = router;
