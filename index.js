1ceb8ba30d927b45b35be50ed180d0729d9dd62c
muddy700 <mohamedmfaume700@gmail.com>
2022-03-08 13:14:45 +0300

    feat: append encounters route into index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const medicinesRouter = require("./routes/medicine");
 const diagnosisRouter = require("./routes/diagnosis");
 const appointmentsRouter = require("./routes/appointment");
 const labTestsRouter = require("./routes/labTest");
+const encountersRouter = require("./routes/encounter");
 const salesInvoicesRouter = require("./routes/salesInvoice");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser"); //todo: uninstall if not used
@@ -37,12 +38,13 @@ mongoose
     app.use("/api/v1/users", usersRouter);
     app.use("/api/v1/patients", patientsRouter);
     app.use("/api/v1/symptoms", symptomsRouter);
+    app.use("/api/v1/lab-tests", labTestsRouter);
     app.use("/api/v1/diagnosis", diagnosisRouter);
     app.use("/api/v1/medicines", medicinesRouter);
-    app.use("/api/v1/lab-tests", labTestsRouter);
-    app.use("/api/v1/sales-invoices", salesInvoicesRouter);
+    app.use("/api/v1/encounters", encountersRouter);
     app.use("/api/v1/appointments", appointmentsRouter);
     app.use("/api/v1/payment-modes", paymentModesRouter);
+    app.use("/api/v1/sales-invoices", salesInvoicesRouter);
     app.use("/api/v1/lab-test-templates", labTestTemplateRouter);
 
     app.listen(PORT, () => {
