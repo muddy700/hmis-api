b9590a438e55d04feace0a267752e0a91343baf2
muddy700 <mohamedmfaume700@gmail.com>
2022-03-08 13:14:10 +0300

    feat: add endpoint to get all encounters

--- routes/encounter.js
+++ routes/encounter.js
@@ -2,4 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Encounter = require("../models/Encounter");
 
+//Get All Encounters
+router.get("/", async (req, res) => {
+  try {
+    const encounters = await Encounter.find();
+    res.status(200).send(encounters);
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
 module.exports = router;
