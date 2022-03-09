0527360900a93db81987508e772fa0bfa2bb9ecb
muddy700 <mohamedmfaume700@gmail.com>
2022-03-09 17:29:01 +0300

    feat: add authentication header

--- authentication.js
+++ authentication.js
@@ -2,19 +2,24 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 const secret_key = process.env.JWT_SECRET_KEY;
+const custom_header = process.env.AUTH_HEADER;
+const allowed_domain = process.env.SOURCE_DOMAIN;
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
+  const request_domain = req.headers[custom_header];
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) return res.status(401).send();
-
-  jwt.verify(token, secret_key, (err, user) => {
-    if (err) return res.status(403).send(err);
-    req.user = user;
-    // console.log(`user: ${user}, Err: ${err}`);
-    next();
-  });
+  else if (!request_domain) return res.status(401).send(request_domain);
+  else if (request_domain !== allowed_domain) return res.status(401).send();
+  else {
+    jwt.verify(token, secret_key, (err, user) => {
+      if (err) return res.status(401).send(err);
+      req.user = user;
+      next();
+    });
+  }
 };
 
 module.exports = authenticateToken;
