6e8ab615c23bbe9f1238e2e2c039b48b1985b12b
muddy700 <mohamedmfaume700@gmail.com>
2022-03-10 12:29:29 +0300

    feat: add endpoint to get users by gender

--- routes/filters.js
+++ routes/filters.js
@@ -3,17 +3,30 @@ const router = express.Router();
 const User = require("../models/User");
 
 //Data Populators
-const userProjector = { role: 1 };
+const userProjector = { password: 0 };
 const rolePopulator = {
   path: "role",
   select: "role_name -_id",
 };
 
-//Get All Users By RoleId
+//Get All Users By Role
 router.get("/roles/:role_id/users", async (req, res) => {
   try {
     const users = await User.find(
       { role: req.params.role_id },
+      userProjector
+    ).populate(rolePopulator);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
+//Get All Users By Gender
+router.get("/users-by-gender", async (req, res) => {
+  try {
+    const users = await User.find(
+      { gender: req.body.gender },
       userProjector
     ).populate(rolePopulator);
     res.status(200).send(users);
