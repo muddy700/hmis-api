4c2e109bbc386f828bd6de3a117bc8c940cea589
muddy700 <mohamedmfaume700@gmail.com>
2022-03-10 12:09:25 +0300

    feat: add endpoint to get users by role

--- routes/filters.js
+++ routes/filters.js
@@ -9,4 +9,17 @@ const rolePopulator = {
   select: "role_name -_id",
 };
 
+//Get All Users By RoleId
+router.get("/roles/:role_id/users", async (req, res) => {
+  try {
+    const users = await User.find(
+      { role: req.params.role_id },
+      userProjector
+    ).populate(rolePopulator);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send({ error: error });
+  }
+});
+
 module.exports = router;
